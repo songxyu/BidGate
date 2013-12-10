7e9946bb555eeeffec060ba5036e322a09867fb5
wikingwei <pubwei@gmail.com>
2013-12-10 22:36:46 +0800

    add assocation between user and order_price_history,  view can show bid user now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
  
   has_many :sell_orders, :class_name => "Order", :foreign_key => 'seller_id' #, dependent: :destroy
   has_many :buy_orders, :class_name => "Order", :foreign_key => 'buyer_id' #, dependent: :destroy
+  
+  has_many :bid_histories, :class_name => "OrderPriceHistory", :foreign_key => 'buyer_id' 
  
   attr_accessible :nickname, :status, :user_type, :company_id
 end
