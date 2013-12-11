3150117cf62f7c0a7ae4c78592efe3930b77d2ee
songxyu <songxyu102@163.com>
2013-12-11 22:38:27 +0800

    add update bid price for order_price_history

--- app/controllers/order_price_histories_controller.rb
+++ app/controllers/order_price_histories_controller.rb
@@ -8,5 +8,22 @@ class OrderPriceHistoriesController < ApplicationController
   def show
     @order = Order.find(params[:order_id])
     @price_history = @order.order_price_histories.find(params[:id])
-  end 
+  end
+
+  def edit
+  	@order = Order.find(params[:order_id])
+  	@price_history = @order.order_price_histories.find(params[:id])
+  end
+
+  def new
+  	@price_history = @order.order_price_histories.find(params[:order_id])
+  end
+
+  def create
+   
+  	@price_history = OrderPriceHistory.new(order_id: params[:order_id], price: params[:bid_price], buyer_id:1, bid_time: Date.new )
+  	@price_history.save
+    redirect_to order_order_price_history_path(:order_id => @price_history.order_id, :id=>@price_history.id)
+  end
+
 end
