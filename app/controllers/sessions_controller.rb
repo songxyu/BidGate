f4d5232db426bd3b646ae29b02c90f9bdfc59508
wikingwei <pubwei@gmail.com>
2014-02-23 16:37:40 +0800

    added reg page; work ok. Need more biz logic.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+require "date"
+
 class SessionsController < ApplicationController
   def new
     respond_to do |format|        
@@ -9,10 +11,21 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email_username], params[:password])
     if user
       session[:user_id] = user.id
+      logger.debug "===== user id= " + user.id.to_s + ", username= " + user.username + " successfully logon! ======"
+      
+      #user = User.find(user.id)
+      user.last_signin_time = DateTime.current
+      user.last_signin_ip = request.remote_ip
+      if user.save()  # TODO: save fail! ?
+        logger.debug "=== updated user login info for user=" + user.to_s
+      else
+        logger.debug "***** Failed to update user login info for user.id=" + user.id.to_s
+      end
+    
       redirect_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
-      render "new"
+      redirect_to new_session_url
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,65 @@
 class UsersController < CommonController
   def new
     @user = User.new
-    common_response
   end
 
   def create
-    
     createdUserInfo = params[:user]
+
+    # handle company reg cases
+    company_acc_num = params[:company][:account_num] # no [:user][:company] !
+    company_legal_person = params[:company][:legal_person]
+    if company_acc_num
+      logger.debug "*** reged company account num= "+ company_acc_num
+      reged_comp = Company.where(account_num: company_acc_num).first # DO NOT miss .first , otherwise, reged_comp.id will error!
+      if reged_comp && reged_comp.legal_person == company_legal_person then
+        createdUserInfo['company_id'] = reged_comp.id
+      else
+        flash.now.alert = "Invalid Company account!"
+        render "new"
+      end
+    else
+      new_company_params = params[:company] # no [:user][:company] !
+      new_company_params["account_num"] = SecureRandom.uuid
+
+      logger.debug "*** new company info= " + new_company_params.to_s
+      new_company = Company.new(new_company_params)
+      if new_company.save
+        createdUserInfo['company_id'] = new_company.id
+      else
+        flash.now.alert = "Fail to create company!"
+        render "new"
+      end
+    end
+
+    # fill more user info
     createdUserInfo['status'] = 0
-    createdUserInfo['company_id'] = 1
     createdUserInfo['signup_time'] = DateTime.current
     createdUserInfo['last_signin_time'] = DateTime.current
-    createdUserInfo['last_signin_ip'] = '0.0.0.0' # TODO: retrieve user client ip            
-    
+    createdUserInfo['last_signin_ip'] = request.remote_ip # retrieve user client ip
+
+    # create the new user
     @user = User.new(createdUserInfo)
-     puts '=========== before encypted password user info: '
-      p( @user )
-      
+    logger.debug '=========== before encypted password user info: '
+    logger.debug @user.to_s
+
     if @user.save
-      puts '=========== after saved, encypted password user info: '
-      p( @user )
+      logger.debug '=========== after saved, encypted password user info: '
+      logger.debug @user.to_s
+
       session[:user_id] = @user.id
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to root_url, :notice => "注册成功!"
     else
       render "new"
     end
   end
-  
+
   def profile
     @user = current_user
     params[:action] = "show"
     common_response
   end
-  
-  
+
   def edit
     @user = current_user
     common_response
