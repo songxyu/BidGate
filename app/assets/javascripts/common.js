c918619579c9d687e863330036a57bf1a5805ce0
wikingwei <pubwei@gmail.com>
2014-02-28 00:53:31 +0800

    add mask div layer for reg form.

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -28,5 +28,33 @@ var fnMsgArea = function(){
 	$(".msg_area").click(function(){
 		$(this).remove();
 	});
-
 };
+
+
+var fnRemoveMaskDiv = function(selector){
+	$(selector).find(".mask-layer").remove(); 
+};
+
+var fnMaskDiv = function(selector, newHeight){
+  if( !$(selector)[0] )
+  	return;
+  	
+  	fnRemoveMaskDiv(selector);
+  
+  var height = newHeight  || $(selector).height() + 50; // 20 for hack
+  var width =  $(selector).width();
+  if(parseInt(height)<=0 || parseInt(width)<=0) {
+    return;
+  }
+  
+  var oPos = $(selector).position(); //.offset(); ? // offsetLeft
+  //alert( "oPos.left="+ oPos.left + ", oPos.top="+ oPos.top);
+  
+  var $div = $('<div  class="mask-layer" style="z-index: 9999; border:medium none; top: '+ oPos.top +'px; left: '+ oPos.left +'px' +'; background-color: #eee; position: absolute; opacity: 0.8;"></div>');
+  $div.css('height', height + 'px');
+  $div.css('width', width+'px'); 
+
+  $(selector).append($div); 
+};
+
+
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,16 +9,26 @@ class CompaniesController < CommonController
     @result = false;
 
     acc_num = params[:account_num]
-    person = params[:legal_person]    
-        
-    if !acc_num || !person then
-      render :template => "companies/verify.js.erb" and return
-    end
+    person = params[:legal_person]
 
-    logger.debug "companies.verify: acc_num="+ acc_num +", legal person="+ person
-    reged_comp = Company.check_existing( acc_num, person)
-    if reged_comp
-      @result = true
+    # new company
+    comp_name = params[:name]
+    comp_license = params[:license_image]
+    comp_addr = params[:register_address]
+
+    if !acc_num then
+      if comp_name && comp_license && person && comp_addr then
+        # TODO: check the license, new company
+        @result = true
+      end
+    else
+      if person then
+        logger.debug "companies.verify: acc_num="+ acc_num +", legal person="+ person
+        reged_comp = Company.check_existing( acc_num, person)
+        if reged_comp
+          @result = true
+        end
+      end
     end
 
     render :template => "companies/verify.js.erb" and return
