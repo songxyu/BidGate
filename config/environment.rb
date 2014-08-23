90f399842458ccc864e9d2015e4d28f3ce79faad
wikingwei <pubwei@gmail.com>
2014-08-23 22:36:52 +0800

    fixed environment.rb config errors for payment gems. Using activemerchant (1.42.2) Using activemerchant_patch_for_china (0.2.1)

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,16 @@ BidGate::Application.initialize!
 
 
 # config for activemerchant and activemerchant_patch_for_china as gems
-config.gem "activemerchant", :lib => "active_merchant"
-config.gem "activemerchant_patch_for_china", :lib => false
+BidGate::Application.config.gem "activemerchant", :lib => "active_merchant"
+BidGate::Application.config.gem "activemerchant_patch_for_china", :lib => false
 
 
 require 'active_merchant'
 require 'active_merchant/billing/integrations/action_view_helper'
 
 ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
+
+# TODO: your api key, account and email 
+ActiveMerchant::Billing::Integrations::Alipay::KEY = "xxxxx"
+ActiveMerchant::Billing::Integrations::Alipay::ACCOUNT = "account-xxx"
+ActiveMerchant::Billing::Integrations::Alipay::EMAIL  = "bid@test.com"
